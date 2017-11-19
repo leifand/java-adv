6e0e057d61d5e60fc72fd3951469ebd70a613e86
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-18 23:54:53 -0600

    BankAccount initial commit

--- src/com/la/bankaccount/BankAccount.java
+++ src/com/la/bankaccount/BankAccount.java
@@ -5,5 +5,14 @@ public class BankAccount {
 	private static int totalAccounts = 0;
 	private static double totalDeposits = 0.0;
 	
+	private String accountNumber = null;
+	private double checkingBalance = 0.0;
+	private double savingsBalance = 0.0;
+	
 	public BankAccount() {;}
+	
+	private String generateAccountNumber() { return "ihateeclipse";}
+	
+	
+	
 }
