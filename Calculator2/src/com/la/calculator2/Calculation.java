85e60913040cdc821399c071357b5b2636bb06f8
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-19 23:09:39 -0600

    Calculator 2 initial commit

---
+++ Calculator2/src/com/la/calculator2/Calculation.java
@@ -0,0 +1,10 @@
+package com.la.calculator2;
+
+public class Calculation {
+	private Calculation opA = null;
+	private Calculation opB = null;
+	private String operator = "";
+	private double result = 0.0;
+	private boolean solved = false;
+	private boolean firstOrder = false;
+}
---
+++ Calculator2/src/com/la/calculator2/Calculator2.java
@@ -0,0 +1,12 @@
+package com.la.calculator2;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Calculator2 implements Serializable {
+	private static final long serialVersionUID = -3873201774388689353L;
+	private ArrayList<Object> operations = new ArrayList<Object>();
+	
+	public Calculator2() {;}
+	
+}
