afb65f785dcd9f49613cddbcf7ac6fb4f5d4a22c
Leif Anderson <leif.v.anderson@gmail.com>
2017-11-19 19:42:14 -0600

    Bank Account

--- src/com/la/bankaccount/BankAccount.java
+++ src/com/la/bankaccount/BankAccount.java
@@ -1,29 +1,41 @@
 package com.la.bankaccount;
 
-import java.util.Random;
-
 public class BankAccount {
 	
 	private static int totalAccounts = 0;
 	private static double totalDeposits = 0.0;
-	private static String nextAccountNumber;
 	
-	private String accountNumber;
+	private String accountNumber = "";
 	private double checkingBalance = 0.0;
 	private double savingsBalance = 0.0;
 
-	static {
-		Random generator = new Random();
-		for (int i = 0; i < 10; i++) { 
-			nextAccountNumber += generator.nextInt(10); 
-		}
+	public BankAccount() {
+		totalAccounts++;
+		accountNumber = generateAccountNumber();
 	}
 	
-	public BankAccount() {;}
+	private String generateAccountNumber() { // from solution, my static initialization was too complicated
+		String acc = "";
+		int max = 10;
+		int min = 0;
+
+		for(int i = 0; i < 10; i++) {
+			int random = (int)(Math.random() * max + min);
+			acc = acc.concat(String.valueOf(random));
+		}
+		return acc;
+	}
 	
-	private static String generateAccountNumber() { return nextAccountNumber;}
-	public void setAccountNumber() { accountNumber = generateAccountNumber(); }
 	public String getAccountNumber() { return accountNumber; }
 	public double getCheckingBalance() { return checkingBalance; }
 	public double getSavingsBalance() { return savingsBalance; }
+	
+	public void depositChecking(double amount) {
+		checkingBalance += amount;
+		totalDeposits += amount;
+	}
+	public void depositSavings(double amount) {
+		savingsBalance += amount;
+		totalDeposits += amount;
+	}
 }
--- src/com/la/bankaccount/BankAccountTest.java
+++ src/com/la/bankaccount/BankAccountTest.java
@@ -7,9 +7,6 @@ public class BankAccountTest {
 		BankAccount a1 = new BankAccount();
 		BankAccount a2 = new BankAccount();
 		
-		a1.setAccountNumber();
-		a2.setAccountNumber();
-		
 		System.out.println(a1.getAccountNumber());
 		System.out.println(a2.getAccountNumber());
 
